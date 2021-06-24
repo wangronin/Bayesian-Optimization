9958f7dbd1a04b5f15a870751856df4402590fad
Hao Wang <wangronin@gmail.com>
2021-06-24 08:09:53 +0200

    Delete bayes_optim/Surrogate directory

--- bayes_optim/Surrogate/__init__.py
+++
@@ -1,4 +0,0 @@
-from .gaussian_process import GaussianProcess, trend
-from .random_forest import RandomForest, SurrogateAggregation
-
-__all__ = ["GaussianProcess", "RandomForest", "SurrogateAggregation", "trend"]
