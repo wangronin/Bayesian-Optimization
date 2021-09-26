9775c48ecc6da3256b5bc0312d5336f6dfd3bc07
wangronin <wangronin@gmail.com>
2021-09-26 18:25:16 +0200

    update

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         "threadpoolctl>=2.1.0",
         "requests>=2.25.0",
         "requests-oauthlib>=1.3.0",
-        "gpytorch>=1.4.2",
         "sobol-seq>=0.2.0",
         "py-expression-eval>=0.3.13",
     ],
