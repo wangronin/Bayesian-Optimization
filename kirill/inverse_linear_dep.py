d8689c3de0dba82d899b033f76a0804cc52b9acd
Kirill Antonov <kirant9797@gmail.com>
2022-02-21 16:25:55 +0100

    add implementation of the inverse transform as in the paper

--- kirill/inverse_linear_dep.py
+++ kirill/inverse_linear_dep.py
@@ -6,6 +6,7 @@ import numpy as np
 from kirill.utils import *
 from functools import partial 
 from scipy import optimize
+from math import exp
 
 
 def l2(x):
@@ -27,47 +28,97 @@ def f(X,k,V,z_star,sz,w):
     g_star = [0.] * len(X)
     for i in range(len(X)):
         g_star[i] = k(X[i], candidate_x)
-    return l2(np.transpose(np.array(z_star[:sz])) - np.matmul(V, np.array(g_star)))
+    return l2(np.transpose(np.array(z_star[:sz])) - np.matmul(V, np.array(g_star))) 
+
+
+def sorted_eig(X):
+    values, vectors = np.linalg.eig(X)
+    values_ids = [(v,i) for i,v in enumerate(values)]
+    values_ids.sort()
+    values_ids = values_ids[::-1]
+    sorted_vectors = deepcopy(vectors)
+    sorted_values = deepcopy(values)
+    cnt = 0
+    for v,i in values_ids:
+        for j in range(len(vectors)):
+            sorted_vectors[j][cnt] = vectors[j][i]
+        cnt += 1
+    for i in range(len(values)):
+        sorted_values[i],_ = values_ids[i]
+    return sorted_values, sorted_vectors
+
+
+def rbf(gamma, a, b):
+    return exp(-gamma*np.sum((np.array(a)-np.array(b))**2))
+
+
+def get_gram_line(k, X, p):
+    return np.array([k(p, x) for x in X])
 
 
 def run_experiment():
     dim = 2
-    lb, ub = -5, 5
+    lb, ub = -10, 10
     DOESIZE = 5
     space = RealSpace([lb, ub],random_seed=0) * dim
-    kpca = KernelPCA(kernel="rbf", gamma=0.01)
+    # kpca = KernelPCA(kernel="rbf", gamma=0.01)
     X, Y, colours = sample_doe(lb, ub, dim, DOESIZE, bn.F17())
-    # X_weighted = get_rescaled_points(X, Y)
-    X_weighted = X
-    kpca.fit(X_weighted)
+    X_weighted = get_rescaled_points(X, Y)
+    # X_weighted = X
+    # kpca.fit(X_weighted)
+    my_kernel = partial(rbf, 0.01)
     
-    G = [[kernel_function(kpca, x1, x2) for x1 in X_weighted] for x2 in X_weighted]
+    G = [[my_kernel(x1, x2) for x1 in X_weighted] for x2 in X_weighted]
     G1 = center_G(G)
     eprintf("G is\n", np.array(G))
     eprintf("G1 is\n", np.array(G1))
 
-    eigenValues, eignVectors = np.linalg.eig(G1)
+    eignValues, eignVectors = sorted_eig(G1)
+    eprintf("eigen values", eignValues)
+    eprintf("sum of eigenvalues is", sum(eignValues))
+    epsilon = 0.2
+    eignValuesSum = sum(eignValues)
+    s=0
+    k=0
+    while s<=(1-epsilon)*eignValuesSum:
+        eprintf(f"percentage of first {k} components is {s/eignValuesSum*100.}")
+        s += eignValues[k]
+        k += 1
+    
+    eprintf(f"percentage of first {k} components is {s/eignValuesSum*100.}")
+    eprintf("k equals to", k)
+    eignValueMatrix = deepcopy(eignVectors)
+    for i in range(len(eignValueMatrix)):
+        for j in range(len(eignValueMatrix)):
+            if i != j: eignValueMatrix[i][j] = 0.
+            else: eignValueMatrix[i][j] = eignValues[i]
+
+    eprintf("V*L*V^T should be equal to G1:\n", np.matmul(np.matmul(eignVectors, eignValueMatrix), np.transpose(eignVectors)))
 
     V = np.transpose(eignVectors)
-    k = 4
-    eprintf("V", V)
+    eprintf("V\n", V)
     V = V[:k]
-    eprintf("\n", V)
+    eprintf("V reduced\n", V)
     
     TEST = 2
     for i in range(TEST):
-        if i==1:
-            p=[x1]
-        else:
+        if i == 0:
             p = space._sample(1)
-        eprintf(p)
-        y = kpca.transform(p)[0]
-        partial_f = partial(f, X, partial(kernel_function, kpca), V, y, k)
+        else:
+            p = x1
+        # eprintf('initial point is', p)
+        # y = kpca.transform(p)[0]
+        g = get_gram_line(my_kernel, X, p)
+        # eprintf("g(p) is ", g)
+        y = (V @ np.transpose(g))[:k]
+        # eprintf("point in the feature space", y)
+        partial_f = partial(f, X, my_kernel, V, y, k)
         w0, fopt,*rest = optimize.fmin_bfgs(partial_f, np.zeros(len(X)), full_output=True, disp=False)
-        eprintf("fopt", fopt)
-        eprintf("w0", w0)
-        eprintf(linear_combination(w0, X))
-        x1 = linear_combination(w0, X)
+        # eprintf("fopt", fopt)
+        # eprintf("w0", w0)
+        p1 = linear_combination(w0, X)
+        eprintf('initial point is', p, 'I(\Phi(x)) is', p1)
+        x1 = p1
 
 
 def center_G(G):
