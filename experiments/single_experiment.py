2ae71e143fea16027e4479e5c9a8a557c2a62a9c
Kirill Antonov <kirant9797@gmail.com>
2022-04-11 18:12:34 +0200

    update

--- experiments/single_experiment.py
+++ experiments/single_experiment.py
@@ -23,6 +23,7 @@ lb, ub = -5, 5
 
 
 def create_algorithm(optimizer_name, func, dim, total_budget, doe_size):
+    global seed
     seed = random.randint(1, 1e9)
     space = RealSpace([lb, ub], random_seed=seed) * dim
     print(f'seed={seed}')
@@ -60,7 +61,7 @@ def create_algorithm(optimizer_name, func, dim, total_budget, doe_size):
             max_FEs=total_budget,
             verbose=False,
             n_point=1,
-            n_components=0.95,
+            n_components=0.90,
             acquisition_optimization={"optimizer": "OnePlusOne_Cholesky_CMA"},
         )
     elif optimizer_name == 'BO':
