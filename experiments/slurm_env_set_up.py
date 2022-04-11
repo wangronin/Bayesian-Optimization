6308594c5d2117e3503a722922d1910c093a6735
Kirill Antonov <kirant9797@gmail.com>
2022-04-11 16:41:08 +0200

    add random seed

--- experiments/slurm_env_set_up.py
+++ experiments/slurm_env_set_up.py
@@ -9,7 +9,7 @@ class ExperimentEnvironment:
     SLURM_SCRIPT_TEMPLATE = '''#!/bin/env bash
 
 #SBATCH --job-name=##folder##
-#SBATCH --array=##from_number##-##to_number##
+#SBATCH --array=0-##jobs_count##
 #SBATCH --partition=cpu-long
 #SBATCH --mem-per-cpu=1G
 #SBATCH --time=7-00:00:00
@@ -20,7 +20,9 @@ class ExperimentEnvironment:
 #SBATCH --output=##logs_out##
 #SBATCH --error=##logs_err##
 
-python ../single_experiment.py configs/${SLURM_ARRAY_TASK_ID}.json
+num=##from_number##
+FILE_ID=$((${SLURM_ARRAY_TASK_ID}+$num))
+python ../single_experiment.py configs/${FILE_ID}.json
 '''
 
     def __init__(self):
@@ -55,7 +57,7 @@ python ../single_experiment.py configs/${SLURM_ARRAY_TASK_ID}.json
             with open(os.path.join(self.experiment_root, f'slurm{self.__number_of_slurm_scripts}.sh'), 'w') as f:
                 f.write(script\
                         .replace('##from_number##', str(offset))\
-                        .replace('##to_number##', str(offset + self.__max_array_size - 1)))
+                        .replace('##jobs_count##', str(self.__max_array_size - 1)))
             offset += self.__max_array_size
             self.__number_of_slurm_scripts += 1
         r = self.generated_configs % self.__max_array_size
@@ -63,8 +65,8 @@ python ../single_experiment.py configs/${SLURM_ARRAY_TASK_ID}.json
             with open(os.path.join(self.experiment_root, f'slurm{self.__number_of_slurm_scripts}.sh'), 'w') as f:
                 f.write(script\
                         .replace('##from_number##', str(offset))\
-                        .replace('##to_number##', str(offset + r - 1)))
-            offset += self.__max_array_size
+                        .replace('##jobs_count##', str(r - 1)))
+            offset += r
             self.__number_of_slurm_scripts += 1
 
     def __generate_configs(self, experiment_config_file_name):
